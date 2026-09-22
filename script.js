3594d5698b4baf43c60360b24501e60391819c94
Mbo boua <mboboua1@gmail.com>
2026-09-22 21:47:57 +0000

    version1 de la calculatrice

---
+++ script.js
@@ -0,0 +1,6 @@
+//etape1 : recuperer les boutton saisir sur l'ecran 
+const ecran = document.querySelector('.ecran');
+const boutons = document.querySelectorAll('.bouton');
+let valeurAtuelle = '0';
+let resultat = false;   // POUR SAVOIR SI LE RESULTAT EST AFFICHER OU PAS
+
